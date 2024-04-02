8b16e2b5b17424f2bb779b975df6de81eec4b852
Theo8338 <teosandu88@gmail.com>
2024-04-02 15:06:32 +0300

    Initial commit

---
+++ Timer.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Time.h"
+#include "Button.h"
+class Timer: public Button, public Clock
+{
+private: 
+
+	const float xbox = 60;
+	const float ybox = 40;
+	Button Start;
+	Button Stop;
+	Button Reset;
+	sf::Text StopWatch;
+public:
+	Timer(sf::RenderWindow& window, sf::FloatRect textRect);
+	void render(sf::RenderWindow& target);
+	void reset();
+	void update(sf::Vector2f mousePos, Timer& timer,Clock& time, sf::RenderWindow& window);
+	sf::Text stopwatch();
+
+};
+
---
+++ teste.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+
+
+class Alarm
+{
+private:
+	sf::RectangleShape hours;
+	sf::RectangleShape minutes;
+	sf::CircleShape toggle;
+
+};
+
+int main()
+{
+
+}
